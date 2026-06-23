ccda0142cdbe9644fcf5f35dfd6d87be026966a8
ghomb <10456048+ghomb@users.noreply.github.com>
2026-06-24 02:10:02 +0300

    Change upstream parameter to 'ghomb/scoop-fixes:master'

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "ghomb/scoop-fixes:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
